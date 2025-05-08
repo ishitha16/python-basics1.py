7e2ae7103f8ad111506e1fec4374bc9fd1d65104
ishitha16 <javvadiishi@gmail.com>
2025-05-08 07:55:40 +0200

    Create age.py
    If the person is 18 and above 18 are eligible to vote

---
+++ age.py
@@ -0,0 +1,9 @@
+#code for if condition to check,whether the person is eligible to work or not
+#age>18
+
+age=int(input("Enter the age of the person:"))
+if age>=18:
+    print("eligible to vote")
+else:
+    print("not eligible to vote")
+    
